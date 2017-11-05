f81697cfbb0af14c88bfdedc57a465a234844f0f
Paweł Tobiszewski <pawel.tobiszewski@gmail.com>
2017-11-05 22:35:47 +0100

    Add LeakCanary library for leak detection

--- app/src/main/java/com/tobikster/medicreminder/MedicReminderApplication.java
+++ app/src/main/java/com/tobikster/medicreminder/MedicReminderApplication.java
@@ -2,12 +2,22 @@ package com.tobikster.medicreminder;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import timber.log.Timber;
 
 public class MedicReminderApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+
+		if (LeakCanary.isInAnalyzerProcess(this)) {
+			// This process is dedicated to LeakCanary for heap analysis.
+			// You should not init your app in this process.
+			return;
+		}
+
+		LeakCanary.install(this);
 
 		if (BuildConfig.DEBUG) {
 			Timber.plant(new Timber.DebugTree());
